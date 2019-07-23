7625a13e15a3c6830629293aec185df95d2664a9
Smembe812 <psemberekajr@gmail.com>
2019-07-23 09:57:01 +0200

    added sidebar

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    debugger
+  document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
